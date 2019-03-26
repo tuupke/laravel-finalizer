17a0fc938fface374f9ec4b4cb687e27804aaba9
Mart Pluijmaekers <mart@tuupke.nl>
2019-03-26 13:50:09 +0100

    Added property to enforce async handling, even when an error is thrown; will only be used when fastcgi is used

--- src/Finalizer.php
+++ src/Finalizer.php
@@ -2,21 +2,28 @@
 
 class Finalizer {
 
-    private $closures = [];
+    private $closures   = [];
+    private $syncOnExit = false;
 
     /**
      * Finalizer constructor. Registers the shutdown function
      */
-    public final function __construct () {
+    public final function __construct() {
         register_shutdown_function(function ($obj) {
+            if (!$this->syncOnExit && function_exists("fastcgi_finish_request")) {
+                fastcgi_finish_request();
+            }
+
             $closurePriorities = $obj->closures;
 
             // Sort by priorities
             ksort($closurePriorities);
 
-            foreach ($closurePriorities as $priority => $closures)
-                foreach ($closures as $closure)
+            foreach ($closurePriorities as $priority => $closures) {
+                foreach ($closures as $closure) {
                     $closure();
+                }
+            }
         }, $this);
     }
 
@@ -24,12 +31,18 @@ class Finalizer {
      * Register a closure to execute after Call has finished.
      *
      * @param \Closure $cb The closure to call.
-     * @param int $priority The priority of execution.
+     * @param int      $priority The priority of execution.
      */
-    public final function register (\Closure $cb, int $priority = 100) {
-        if (php_sapi_name() === 'cli') // running from console/daemon
+    public final function register(\Closure $cb, int $priority = 100) {
+        // running from console/daemon
+        if (php_sapi_name() === 'cli') {
             $cb();
-        else
+        } else {
             $this->closures[$priority][] = $cb;
+        }
+    }
+
+    public final function inSyncOnExit(bool $sync = true) {
+        $this->syncOnExit = $sync;
     }
 }
